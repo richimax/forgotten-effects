3bc7fc6d76d8bab0f2f0b634c422ba5ee48d9dbc
richimax <ric4rd03098@gmail.com>
2016-04-11 02:55:09 -0300

    First commit

---
+++ R/square.R
@@ -0,0 +1,10 @@
+#' A basic example
+#' 
+#' very nice
+#' this is very goood
+#' @param x A numeric value to be squared
+#' @return The square of a number
+#' @export
+square <- function(x) {
+    return (x^2)
+}
